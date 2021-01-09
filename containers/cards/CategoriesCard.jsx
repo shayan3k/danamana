b1f732be3b25177f071e3bfc5d5f373b26e9a800
Mahdi <mahditahavorgar@gmail.com>
2021-01-09 17:04:16 +0330

    UPDATE: categories

--- containers/cards/CategoriesCard.jsx
+++ containers/cards/CategoriesCard.jsx
@@ -5,8 +5,8 @@ export default function CategoriesCard({ categories }) {
     return (
         <Row justify="center" gutter={[0, 0]}>
         {
-            categories.map((data) => {
-                return <Col span={20}><CategoryCard src={data.src} title={data.title} /></Col>
+            categories.map((data, index) => {
+                return <Col key={index} span={20}><CategoryCard src={data.src} title={data.title} color={data.color}/></Col>
             })
         }
         </Row>
