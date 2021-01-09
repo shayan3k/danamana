683e0d8be9a301019572fafe520540d2722c0478
Shahriyar <shahryar.tarnasi@gmail.com>
2021-01-09 13:04:10 +0330

    add fonts base

--- pages/index.js
+++ pages/index.js
@@ -12,44 +12,6 @@ export default function Home() {
     <Layout>
       {/* Base Content */}
       <h1>سلام بچهای کیری خوش امدید کونی ها</h1>
-      <OwlCarousel className="owl-theme" loop margin={10} nav>
-        <div class="item">
-          <h4>1</h4>
-        </div>
-        <div class="item">
-          <h4>2</h4>
-        </div>
-        <div class="item">
-          <h4>3</h4>
-        </div>
-        <div class="item">
-          <h4>4</h4>
-        </div>
-        <div class="item">
-          <h4>5</h4>
-        </div>
-        <div class="item">
-          <h4>6</h4>
-        </div>
-        <div class="item">
-          <h4>7</h4>
-        </div>
-        <div class="item">
-          <h4>8</h4>
-        </div>
-        <div class="item">
-          <h4>9</h4>
-        </div>
-        <div class="item">
-          <h4>10</h4>
-        </div>
-        <div class="item">
-          <h4>11</h4>
-        </div>
-        <div class="item">
-          <h4>12</h4>
-        </div>
-      </OwlCarousel>;
     </Layout>
   )
 }
